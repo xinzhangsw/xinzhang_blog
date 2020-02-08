d859f3e8c9ec9924ed2f037b9317af671005e055
xinzhangsw <jinxinzhangsw@163.com>
2020-02-08 23:37:04 +0800

    auto generate article excerpt

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,13 @@
+import markdown
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.urls import reverse
+from django.utils.html import strip_tags
 
 
 # Create your models here.
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -47,6 +50,11 @@ class Post(models.Model):
 
     def save(self, *args, **kwargs):
         self.modified_time = timezone.now()
+        md = markdown.Markdown(extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+        ])
+        self.excerpt = strip_tags(md.convert(self.body))[:54]
         super().save(*args, **kwargs)
 
     def get_absolute_url(self):
