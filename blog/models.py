bdbe4450e710ac05f29f8e4d2a4ce4fd2b358b98
xinzhangsw <jinxinzhangsw@163.com>
2020-02-09 17:29:08 +0800

    auto count increase views

--- blog/models.py
+++ blog/models.py
@@ -40,6 +40,7 @@ class Post(models.Model):
     category = models.ForeignKey(Category, verbose_name='分类', on_delete=models.CASCADE)
     tag = models.ManyToManyField(Tag, verbose_name='标签', blank=True)
     author = models.ForeignKey(User, verbose_name='作者', on_delete=models.CASCADE)
+    views = models.PositiveIntegerField(default=0, editable=False)
 
     class Meta:
         verbose_name = '文章'
@@ -59,3 +60,7 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:detail', kwargs={'pk': self.pk})
+
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
